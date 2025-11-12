dc87a2550a4660751e958133a0845bbc91ed37b8
gaboesquivel <contact@gaboesquivel.com>
2025-11-12 18:21:04 +0000

    fix: pulumi docker version

--- infra/vencura/bin/lib/artifact-registry.d.ts
+++
@@ -1,8 +0,0 @@
-import * as gcp from '@pulumi/gcp';
-import type { Config } from './config';
-import type { ServiceAccountResources } from './service-accounts';
-export interface ArtifactRegistryResources {
-    repository: gcp.artifactregistry.Repository;
-}
-export declare function createArtifactRegistry(config: Config, serviceAccounts: ServiceAccountResources, provider: gcp.Provider): ArtifactRegistryResources;
-//# sourceMappingURL=artifact-registry.d.ts.map
